960fdd24f65c5bb9aef8f3f850c0ef47e48c23ee
starjara <starjara@pusan.ac.kr>
2024-08-31 16:16:37 +0900

    Multi-domain

---
+++ .openssl/include/openssl/verse_prot.h
@@ -0,0 +1,13 @@
+#ifndef __VERSE_PROT_H__
+#define __VERSE_PROT_H__
+
+#include "verse.h"
+
+#define AES_BASE 0x10000
+#define GCM_BASE 0xA0000
+//#define AES_INDEX_OFFSET  24
+#define AES_INDEX_OFFSET  20
+
+extern int session_count;
+
+#endif
